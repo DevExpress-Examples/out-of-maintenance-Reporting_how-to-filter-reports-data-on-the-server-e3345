b6f8227e55725c5f4d6eed97b0235e90fd2ac15b
DevExpressExampleBot <fakeEmail>
2021-10-06 15:55:33 +0300

    Source auto update [skip ci]

--- VB/ServerParams/Main.Designer.vb
+++ VB/ServerParams/Main.Designer.vb
@@ -1,58 +1,61 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
 Namespace ServerParams
-	Partial Public Class Main
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Main
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.buttonShow = New DevExpress.XtraEditors.SimpleButton()
-			Me.SuspendLayout()
-			' 
-			' buttonShow
-			' 
-			Me.buttonShow.Anchor = (CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) Or System.Windows.Forms.AnchorStyles.Left) Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles))
-			Me.buttonShow.Location = New System.Drawing.Point(12, 12)
-			Me.buttonShow.Name = "buttonShow"
-			Me.buttonShow.Size = New System.Drawing.Size(252, 69)
-			Me.buttonShow.TabIndex = 0
-			Me.buttonShow.Text = "Show Report"
-'			Me.buttonShow.Click += New System.EventHandler(Me.buttonShow_Click);
-			' 
-			' Main
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(276, 93)
-			Me.Controls.Add(Me.buttonShow)
-			Me.Name = "Main"
-			Me.Text = "Server Parameter Example"
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+'#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.buttonShow = New DevExpress.XtraEditors.SimpleButton()
+            Me.SuspendLayout()
+            ' 
+            ' buttonShow
+            ' 
+            Me.buttonShow.Anchor = CType(((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) Or System.Windows.Forms.AnchorStyles.Left) Or System.Windows.Forms.AnchorStyles.Right)), System.Windows.Forms.AnchorStyles)
+            Me.buttonShow.Location = New System.Drawing.Point(12, 12)
+            Me.buttonShow.Name = "buttonShow"
+            Me.buttonShow.Size = New System.Drawing.Size(252, 69)
+            Me.buttonShow.TabIndex = 0
+            Me.buttonShow.Text = "Show Report"
+             ''' Cannot convert AssignmentExpressionSyntax, System.NullReferenceException: Object reference not set to an instance of an object.
+'''    at ICSharpCode.CodeConverter.VB.NodesVisitor.VisitAssignmentExpression(AssignmentExpressionSyntax node) in C:\work\Sandbox\Yarik\CSharpToVB.root\CSharpToVB\CodeConverter-master\CodeConverter\VB\NodesVisitor.cs:line 1211
+'''    at Microsoft.CodeAnalysis.CSharp.CSharpSyntaxVisitor`1.Visit(SyntaxNode node)
+'''    at ICSharpCode.CodeConverter.VB.CommentConvertingVisitorWrapper`1.Accept(SyntaxNode csNode, Boolean addSourceMapping) in C:\work\Sandbox\Yarik\CSharpToVB.root\CSharpToVB\CodeConverter-master\CodeConverter\VB\CommentConvertingVisitorWrapper.cs:line 26
+''' 
+''' Input:
+'''             this.buttonShow.Click += new System.EventHandler(this.buttonShow_Click)
+'''  ' 
+            ' Main
+            ' 
+Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(276, 93)
+            Me.Controls.Add(Me.buttonShow)
+            Me.Name = "Main"
+            Me.Text = "Server Parameter Example"
+            Me.ResumeLayout(False)
+        End Sub
 
-		Private WithEvents buttonShow As DevExpress.XtraEditors.SimpleButton
-	End Class
+'#End Region
+        Private buttonShow As DevExpress.XtraEditors.SimpleButton
+    End Class
 End Namespace
-
--- VB/ServerParams/ProductReport.vb
+++ VB/ServerParams/ProductReport.vb
@@ -1,23 +1,19 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
 Imports System.Drawing
-Imports System.Collections
 Imports System.ComponentModel
 Imports DevExpress.XtraReports.UI
 Imports DevExpress.XtraReports.Parameters
 
 Namespace ServerParams
-	Partial Public Class ProductReport
-		Inherits DevExpress.XtraReports.UI.XtraReport
-		Public Sub New()
-			InitializeComponent()
-		End Sub
 
-		Private Sub ProductReport_ParametersRequestSubmit(ByVal sender As Object, ByVal e As ParametersRequestEventArgs) Handles MyBase.ParametersRequestSubmit
-			If Parameters("CategoryID") IsNot Nothing Then
-				productsTableAdapter.FillByCategoryID(northwind1.Products, CInt(Fix(Parameters("CategoryID").Value)))
-			End If
-		End Sub
+    Public Partial Class ProductReport
+        Inherits DevExpress.XtraReports.UI.XtraReport
 
-	End Class
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub ProductReport_ParametersRequestSubmit(ByVal sender As Object, ByVal e As ParametersRequestEventArgs)
+            If Parameters("CategoryID") IsNot Nothing Then productsTableAdapter.FillByCategoryID(northwind1.Products, CInt(Parameters(CStr("CategoryID")).Value))
+        End Sub
+    End Class
 End Namespace
